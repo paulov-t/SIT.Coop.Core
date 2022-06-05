b652ce1de274ce6cec109ccf7001be72ab85a20e
PARGSoft <pargsoft@hotmail.com>
2022-06-05 13:10:51 +0100

    new movement replication logic (not great tbh)

--- Player/PlayerOnMovePatch.cs
+++ Player/PlayerOnMovePatch.cs
@@ -30,83 +30,74 @@ namespace SIT.Coop.Core.Player
             return method;
         }
 
-        //[PatchPrefix]
-        //public static void PatchPrefix(object gesture)
-        //{
-        //    Logger.LogInfo("OnGesturePatch.PatchPrefix");
-        //}
-
         public static DateTime? LastMoveTime { get; set; }
         public static DateTime? LastAIMovesTime { get; set; }
 
         public static List<Vector2> StoredMoves = new List<Vector2>();
         public static DateTime? LastStoredMoveTime { get; set; }
 
+        [PatchPrefix]
+        public static bool PatchPrefix(
+            EFT.Player __instance,
+            Vector2 direction)
+        {
+            return true;
+        }
 
         [PatchPostfix]
         public static void PatchPostfix(
             EFT.Player __instance,
             Vector2 direction)
         {
+            if (direction == Vector2.zero)
+                return;
 
             Dictionary<string, object> dictionary = new Dictionary<string, object>();
             dictionary.Add("nP", __instance.Transform.position);
             dictionary.Add("dX", direction.x.ToString());
             dictionary.Add("dY", direction.y.ToString());
+            dictionary.Add("vel", __instance.MovementContext.Velocity);
+            dictionary.Add("spd", __instance.MovementContext.CharacterMovementSpeed);
+            dictionary.Add("mdX", __instance.MovementContext.MovementDirection.x);
+            dictionary.Add("mdY", __instance.MovementContext.MovementDirection.y);
+            dictionary.Add("sprint", __instance.MovementContext.IsSprintEnabled);
             dictionary.Add("m", "Move");
             Task.Run(delegate
             {
                ServerCommunication.PostLocalPlayerData(__instance, dictionary);
             });
-            //Logger.LogInfo("PlayerOnMovePatch.PatchPostfix");
-            //if (MatchmakerAcceptPatches.IsServer && IsAI)
-            //{
-            //    StoredMoves.Add(direction);
-            //}
+        }
 
-            //var timeChecker = -0.3;
+        public static void MoveReplicated(EFT.Player player, Dictionary<string, object> dict)
+        {
+            try
+            {
+                Vector2 direction = new Vector2(float.Parse(dict["dX"].ToString()), float.Parse(dict["dY"].ToString()));
+                direction.x = Math.Sign(direction.x);
+                direction.y = Math.Sign(direction.y);
+                player.MovementContext.MovementDirection = direction;
 
-            //if (
-            //    //((MatchMakerAcceptScreen.IsServer && (IsMyPlayer || IsAI)) || IsMyPlayer)
-            //    (IsMyPlayer)
-            //    && (!LastMoveTime.HasValue || LastMoveTime < DateTime.Now.AddSeconds(timeChecker))
-            //    )
-            //{
-            //    LastMoveTime = DateTime.Now;
-            //    Dictionary<string, object> dictionary = new Dictionary<string, object>();
-            //    dictionary.Add("nP", Transform.position);
-            //    dictionary.Add("dX", direction.x.ToString());
-            //    dictionary.Add("dY", direction.y.ToString());
-            //    dictionary.Add("m", "Move");
-            //    Task.Run(delegate
-            //    {
-            //        ServerCommunication.PostLocalPlayerData(__instance, dictionary);
-            //    });
-            //}
+                bool enableSprint = false;
+                if (dict.ContainsKey("sprint"))
+                    enableSprint = bool.Parse(dict["sprint"].ToString());
 
-            //if (
-            //    (MatchmakerAcceptPatches.IsServer && IsAI)
-            //    &&
-            //    (!LastAIMovesTime.HasValue || LastAIMovesTime < DateTime.Now.AddSeconds(-0.9))
-            //    )
-            //{
-            //    LastAIMovesTime = DateTime.Now;
-
-            //    Dictionary<string, object> dictionary = new Dictionary<string, object>();
-            //    dictionary.Add("store", StoredMoves);
-            //    dictionary.Add("nP", Transform.position);
-            //    dictionary.Add("m", "AIMoves");
-            //    Task.Run(delegate
-            //    {
-            //        ServerCommunication.PostLocalPlayerData(__instance, dictionary);
-            //    });
-
-            //    StoredMoves.Clear();
-            //}
-
-
-            //Logger.LogInfo("OnGesturePatch.PatchPostfix:Sent");
-
+                player.MovementContext.EnableSprint(enableSprint && direction.y > 0.1f);
+                if (player.MovementContext.IsSprintEnabled)
+                {
+                    player.MovementContext.SetPoseLevel(1f);
+                    if (player.MovementContext.PoseLevel > 0.9f && player.MovementContext.SmoothedCharacterMovementSpeed >= 1f)
+                    {
+                        player.MovementContext.PlayerAnimatorEnableSprint(true);
+                    }
+                }
+                if (dict.ContainsKey("spd"))
+                    player.MovementContext.CharacterMovementSpeed = float.Parse(dict["spd"].ToString());
+                player.MovementContext.PlayerAnimatorEnableInert(false);
+            }
+            catch(Exception ex)
+            {
+                Logger.LogInfo("PlayerOnMovePatch.MoveReplicated:ERROR:" + ex.Message);
+            }
         }
     }
 }
